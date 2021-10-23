647366e28b89085505bc00f7064d175f3c836364
sarodz <samirodrigue@hotmail.com>
2021-10-23 15:00:42 -0400

    initial working version

--- src/sample_deployment/base.py
+++ src/sample_deployment/base.py
@@ -1,3 +1,11 @@
 from pydantic import BaseModel
 
-# class CaliData(BaseModel):
+class CaliData(BaseModel):
+    MedInc: float
+    HouseAge: int
+    AveRooms: float
+    AveBedrms: float
+    Population: int
+    AveOccup: float
+    Latitude: float
+    Longitude: float
--- src/sample_deployment/endpoint.py
+++ src/sample_deployment/endpoint.py
@@ -1,3 +1,20 @@
 from fastapi import FastAPI
+from sample_sklearn.train import main 
+from sample_sklearn.load import load_model
+from sample_deployment.base import CaliData 
+import json
+import numpy as np
 
+# prepare model
+main()
+# load model
+MODEL = load_model()
 app = FastAPI()
+FIELDS = list(CaliData.schema()["properties"].keys())
+
+
+@app.post("/predict")
+def predict(data_input: CaliData):
+    data = np.array([data_input.dict()[field] for field in FIELDS])  
+    prediction = MODEL.predict(data.reshape(1, -1))
+    return {"prediction": round(prediction[0], 4)}
